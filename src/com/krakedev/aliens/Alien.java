cc69c8d5b5e6c92994e5d9602a742cb9c627fecf
DylanRa06 <dylanramones20000@hotmail.com>
2026-08-28 19:15:22 -0500

    Parte 4

--- src/com/krakedev/aliens/Alien.java
+++ src/com/krakedev/aliens/Alien.java
@@ -2,4 +2,29 @@ package com.krakedev.aliens;
 
 public class Alien {
 
+    private int tamanio;
+    private String color;
+    private int numeroOjos;
+    private int numeroBrazos;
+    private int numeroPies;
+    private double precioExtremidad;
+    private double precioOjo;
+    private double precioCuerpo;
+
+    public Alien(int tamanio, String color) {
+
+        if (tamanio < 5) {
+            this.tamanio = 5;
+        } else if (tamanio > 30) {
+            this.tamanio = 30;
+        } else {
+            this.tamanio = tamanio;
+        }
+
+        this.color = color;
+
+        this.precioCuerpo = this.tamanio * 0.20;
+        this.precioExtremidad = this.tamanio * 0.10;
+        this.precioOjo = this.tamanio * 0.05;
+    }
 }
